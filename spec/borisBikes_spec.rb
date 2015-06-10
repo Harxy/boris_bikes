b6d7cb83b5374839a16fe3ee352a6bdf2ac24074
Paul Harker <harkerp@gmail.com>
2015-06-10 12:16:07 +0100

    added relese bike method and working test

--- spec/borisBikes_spec.rb
+++ spec/borisBikes_spec.rb
@@ -4,5 +4,11 @@ describe DockingStation do
   # it "is a kind of dockingstation object" do
   #   expect(DockingStation.new).to eq DockingStation
   # end
+  it "responds to a release bike request" do
+    expect(subject).to respond_to :release_bike
+    end
+  it "respons to a working? request on a bike" do
+    expect(subject).to respond_to :working?
 
+  end
 end
