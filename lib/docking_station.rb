f07a455ce30127c1083d9a2e1ecd851d631b6f19
Paul Harker <harkerp@gmail.com>
2015-06-10 21:23:07 +0100

    Made it so that an argument can be passed to set the capacity, with the default being 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,11 @@ require_relative 'bike'
 
 class DockingStation
   # encapsulate literal in a constant and use the constant everywhere else
-  DEFAULT_CAPACITY = 20
 
   attr_accessor :capacity
 
 
-  def initialize capacity = DEFAULT_CAPACITY
+  def initialize (capacity = 20)
     @bikes_docked = []
     @capacity = capacity
   end
@@ -19,7 +18,6 @@ class DockingStation
 
   def dock(bike)
     fail 'Docking station is full' if full?
-    #@bike = bike
     bikes_docked << bike
 
   end
@@ -29,7 +27,7 @@ class DockingStation
   attr_reader :bikes_docked
 
     def full?
-      bikes_docked.length >= DEFAULT_CAPACITY
+      bikes_docked.length >= capacity
     end
 
     def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it 'has a default capacity' do
-    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-  end
-
   it { is_expected.to respond_to :release_bike}
   it 'releases working bike' do
     subject.dock(Bike.new)
