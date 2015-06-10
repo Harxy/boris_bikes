a0f51fdc41c72c309b3534106ece4f24d39ddc4b
Paul Harker <harkerp@gmail.com>
2015-06-10 17:38:15 +0100

    added test to have a capacity of 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,7 +12,7 @@ describe DockingStation do
 
   describe 'dock' do
     it 'raises an error when dock is full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect {subject.dock(Bike.new)}.to raise_error 'Docking station is full'
     end
   end
