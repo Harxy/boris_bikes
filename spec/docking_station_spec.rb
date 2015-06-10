372ad4dd7c0a75d12557a02111f73c022842f6a4
Paul Harker <harkerp@gmail.com>
2015-06-10 15:16:40 +0100

    added test to check for :dock

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do
-  # it "responds to a release bike request" do
-  #   expect(subject).to respond_to :release_bike
-  # end
 
   it { is_expected.to respond_to :release_bike}
 
   # it "notifies person that no bikes are available" do
   #   expect { bike.working? false }.to raise_error(RuntimeError)
   # end
+  it { is_expected.to respond_to(:dock).with(1).argument}
+
 
 end
