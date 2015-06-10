ca63e58a24be0591f17ec3d38bf606909df7aac9
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-10 18:01:41 +0100

    Add aray to store number of bikes docked at station and capacity of 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,22 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes_docked = []
+    @capacity = 20
+  end
+
   def release_bike
-    fail "No bikes available" unless @bike
-    @bike
+    fail "No bikes available" if @bikes_docked.length == 0
+    @bikes_docked.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bike
+    fail 'Docking station is full' if @bikes_docked.length >= @capacity
+
     @bike = bike
+    @bikes_docked << @bike
+
   end
 
 end
